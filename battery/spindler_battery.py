3d63b0e84773144a7be37ef2f5034a0d5a0b5bc4
Lucas Aramendy <yataylucas@gmail.com>
2023-11-28 13:35:22 -0300

    Upgrade Spindler batteries and Add tire servicing criteria

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,7 +1,15 @@
-# En el archivo battery/spindler_battery.py
-from battery.battery import Battery
+# En el archivo spindler_battery.py
+from datetime import date, timedelta
 
-class SpindlerBattery(Battery):
+class SpindlerBattery:
+    def __init__(self, last_service_date: date):
+        self.last_service_date = last_service_date
+
     def needs_service(self):
-        # Implementa la lógica específica para SpindlerBattery
-        pass
+        # Modificar para requerir servicio después de tres años
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
+        return service_threshold_date <= date.today()
+
+# Tests para SpindlerBattery
+# Se deben escribir pruebas que cubran los casos de requerir y no requerir servicio después de tres años.
+ 
--- car_factory.py
+++ car_factory.py
@@ -11,6 +11,8 @@ from sternman_engine_service_criteria import SternmanEngineServiceCriteria
 from willoughby_engine_service_criteria import WilloughbyEngineServiceCriteria
 from spindler_battery_service_criteria import SpindlerBatteryServiceCriteria
 from nubbin_battery_service_criteria import NubbinBatteryServiceCriteria
+
+from typing import List
  
 class CarFactory:
     @staticmethod
@@ -52,3 +54,28 @@ class CarFactory:
         engine_criteria = CapuletEngineServiceCriteria()
         battery_criteria = NubbinBatteryServiceCriteria()
         return Thovex(engine, battery, "good", engine_criteria, battery_criteria)
+    
+    # Agrego nuevo código 
+    def create_car_with_tires(self, tire_wear_matrix: List[List[float]], model: str):
+        # Implementación de lógica para criterios de mantenimiento de neumáticos
+        if model == "Carrigan":
+            return self.create_carrigan(tire_wear_matrix)
+        elif model == "Octoprime":
+            return self.create_octoprime(tire_wear_matrix)
+        else:
+            raise ValueError("Modelo de neumáticos no reconocido")
+    
+    def create_carrigan(self, tire_wear_matrix: List[List[float]]):
+        # Reparar si uno o más valores en la matriz son >= 0.9
+        if any(any(wear >= 0.9 for wear in row) for row in tire_wear_matrix):
+            return self.create_car_with_tires(tire_wear_matrix, "Carrigan")
+        else:
+            return None  # o podrías devolver un mensaje indicando que no se requiere reparación
+
+    def create_octoprime(self, tire_wear_matrix: List[List[float]]):
+        # Reparar si la suma de los valores en la matriz es >= 3
+        total_wear = sum(sum(row) for row in tire_wear_matrix)
+        if total_wear >= 3:
+            return self.create_car_with_tires(tire_wear_matrix, "Octoprime")
+        else:
+            return None  # o podrías devolver un mensaje indicando que no se requiere reparación
